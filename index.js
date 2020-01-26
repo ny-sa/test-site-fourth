b518d31f62f44045c48430c1780f7d5009e9527c
nysa <msun698@gmail.com>
2020-01-26 14:40:02 +0100

    Did the 'boss challenge'

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.querySelector('a').setAttribute('href', 'https://www.bing.com');
+let rand1 = Math.ceil(Math.random() * 6);
+let rand2 = Math.ceil(Math.random() * 6);
+document.querySelector('.img1').setAttribute('src', `images/dice${rand1}.png`);
+document.querySelector('.img2').setAttribute('src', `images/dice${rand2}.png`);
+
+if (rand1 === rand2) document.querySelector('h1').textContent = 'Draw!';
+else (rand1 > rand2) ? document.querySelector('h1').textContent = '🚩Player 1 Wins!' :
+document.querySelector('h1').textContent = 'Player 2 Wins!🚩';
